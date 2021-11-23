7ff2a43817efa0f09fcc171eba70e28ddda72891
Duy Toan <toanpmo123@gmail.com>
2021-11-23 13:10:15 +0700

    up

--- main/main.js
+++ main/main.js
@@ -1,3 +1,16 @@
 $(window).on("load",function(){
     $(".loader-wrapper").fadeOut("slow");
   });
+
+  $(function () {
+    $('#datetimepicker1').datetimepicker();
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
